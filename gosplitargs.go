487d1029428851eca679d735e5bf3aa17db1cf44
elgs <elgs1980@hotmail.com>
2015-06-27 01:01:41 +0800

    Update.

---
+++ gosplitargs.go
@@ -0,0 +1,53 @@
+// gosplitargs
+package gosplitargs
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SplitArgs(input, separator string) ([]string, error) {
+	//        separator = separator || /\s/g;
+	singleQuoteOpen := false
+	doubleQuoteOpen := false
+	var tokenBuffer []string
+	var ret []string
+
+	arr := strings.Split(input, "")
+	for _, element := range arr {
+		matches, err := regexp.MatchString(separator, input)
+		if err != nil {
+			return nil, err
+		}
+		if element == "'" {
+			if !doubleQuoteOpen {
+				singleQuoteOpen = !singleQuoteOpen
+				continue
+			}
+		} else if element == `"` {
+			if !singleQuoteOpen {
+				doubleQuoteOpen = !doubleQuoteOpen
+				continue
+			}
+		}
+
+		if !singleQuoteOpen && !doubleQuoteOpen {
+			if matches {
+				if len(tokenBuffer) > 0 {
+					ret = append(ret, strings.Join(tokenBuffer, ""))
+					tokenBuffer = make([]string, 0)
+				}
+			} else {
+				tokenBuffer = append(tokenBuffer, element)
+			}
+		} else if singleQuoteOpen {
+			tokenBuffer = append(tokenBuffer, element)
+		} else if doubleQuoteOpen {
+			tokenBuffer = append(tokenBuffer, element)
+		}
+	}
+	if len(tokenBuffer) > 0 {
+		ret = append(ret, strings.Join(tokenBuffer, ""))
+	}
+	return ret, nil
+}
